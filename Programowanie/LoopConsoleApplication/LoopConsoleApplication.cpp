097cecd1fe596253164c408aa62e4c0a3710dd67
IronLittleFox <lukasz.felisek@gmail.com>
2023-11-10 13:41:28 +0100

    koniec lekcji

--- Programowanie/LoopConsoleApplication/LoopConsoleApplication.cpp
+++ Programowanie/LoopConsoleApplication/LoopConsoleApplication.cpp
@@ -5,10 +5,10 @@
 DO-WHILE
 * Napisz program, który policzy sumę cyfr podanej przez użytkownika liczby.
 * Program wyświetlający na ekranie kwadraty liczb od 1 do 10 (np. 1, 4, 9, 16 itd.) dopóki suma tych kwadratów nie przekroczy 1000.
- 
+
 WHILE
 * Miasto T. ma obecnie 100 tys. mieszkańców, ale jego populacja rośnie co roku o 3% rocznie. Miasto B. ma 300 tys. mieszkańców i ta liczba rośnie w tempie 2% na rok. Wykonaj symulację prezentującą liczbę mieszkańców w obu miastach i zatrzymującą się, gdy liczba mieszkańców miasta T. przekroczy liczbę z miasta B.
-* Napisz program, który poprosi użytkownika o wprowadzenie dowolnej liczby całkowitej. Następnie program powinien obliczyć i wyświetlić liczbę cyfr.
+*
 * Program sprawdzający czy podana liczba jest liczbą doskonałą (czyli taką, której suma dzielników (z wyłączeniem samej siebie) jest równa danej liczbie, np. 6 jest liczbą doskonałą, ponieważ 1 + 2 + 3 = 6).
 
 FOR
@@ -20,14 +20,14 @@ FOR
 * Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1=1, 1x2=2, 1x3=3 itd.).
 * Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
 * Program obliczający sumę kwadratów liczb od 1 do 10
-* 
+*
 * Program, kóry wyświetli poniższe cztery zwory:
 	****       54321        121212        122333
 	***        65432        212121        223334444
 	**         76543        121212        333444455555
 	*          87654        212121        444455555666666
 
-* Oblicz sumę szeregu 1/1^2 + 1/2^2 + 1/3^2 + ... +1/n^2. Jeżeli się nie pomylisz, to dla odpowiednio dużej wartości n po przemnożeniu jej przez 6 i spierwiastkowaniu powinieneś otrzymać wartość liczby π (suma szeregu jest równa π2/6). Zwróć uwagę, że pierwsza wartość indeksu to 1, a nie 0. Dokładność obliczeń można sprawdzić porównując z wartością odczytaną z własności Math.PI.
+*
 
 */
 
@@ -146,7 +146,7 @@ void task3()
 void task4()
 {
 	srand(time(NULL));
-	
+
 	int secretNumber = rand() % 10;
 
 	/*std::cout << "Zgadnij liczbe:\n";
@@ -265,14 +265,86 @@ void task7()
 	{
 		for (int number = lineNumber + 1; number <= lineNumber + 3; number++)
 		{
-			for (int i = 0; i < number; i++ )
+			for (int i = 0; i < number; i++)
 				std::cout << number;
 		}
 		std::cout << "\n";
 	}
 }
 
+//Napisz program, który poprosi użytkownika o wprowadzenie dowolnej liczby całkowitej. Następnie program powinien obliczyć i wyświetlić liczbę cyfr.
+void task8()
+{
+	int number = 11;
+
+	//wersja 1
+	int howManyDigits = 1;
+	int upperRange = 10;
+	number = abs(number);
+	while (number > upperRange)
+	{
+		howManyDigits++;
+		upperRange *= 10;
+	}
+	std::cout << howManyDigits << "\n";
+
+	//wersja 2
+	howManyDigits = 1;
+	int tmpNumber = abs(number);
+	while (tmpNumber > 10)
+	{
+		tmpNumber /= 10;
+		howManyDigits++;
+	}
+
+	std::cout << howManyDigits << "\n";
+
+	//wersja 3 ile cyfr binarnie
+	howManyDigits = 1;
+	tmpNumber = abs(number);
+	while (tmpNumber > 1)
+	{
+		tmpNumber >>= 1;
+		howManyDigits++;
+	}
+
+	std::cout << howManyDigits << "\n";
+}
+
+//Oblicz sumę szeregu 1/1^2 + 1/2^2 + 1/3^2 + ... +1/n^2. Jeżeli się nie pomylisz, to dla odpowiednio dużej wartości n po przemnożeniu jej przez 6 i spierwiastkowaniu powinieneś otrzymać wartość liczby π (suma szeregu jest równa π2/6). Zwróć uwagę, że pierwsza wartość indeksu to 1, a nie 0. Dokładność obliczeń można sprawdzić porównując z wartością odczytaną z własności Math.PI.
+void task9()
+{
+	double sum = 0;
+	int n = 2;
+	/*if (n > 1)
+	{
+		sum = sum + 1 / (1.0 * 1.0);
+		if (n > 2)
+		{
+			sum = sum + 1 / (2.0 * 2.0);
+			if (n > 3)
+			{
+				sum = sum + 1 / (3.0 * 3.0);
+				if (n > 4)
+				{
+					sum = sum + 1 / (4.0 * 4.0);
+					//....
+				}
+			}
+		}
+	}*/
+
+	double i = 1;
+	while (n > i)
+	{
+		sum = sum + 1 / (i * i);
+		i++;
+	}
+
+	std::cout << "Suma " << sum << "\n";
+}
+
 int main()
 {
-	task7();
+	task8();
 }
