bad360aed54c279a047d6a32bb0634abb2d15173
IronLittleFox <lukasz.felisek@gmail.com>
2023-11-24 12:56:44 +0100

    dodanie zadań

--- Programowanie/StringConsoleApplication/StringConsoleApplication.cpp
+++ Programowanie/StringConsoleApplication/StringConsoleApplication.cpp
@@ -1,5 +1,21 @@
 ﻿#include <iostream>
 
+/*
+* Napisz program, który będzie prosił o hasło. Nie przepuści dalej dopóki nie zostanie ono podane prawidłowo.
+* Napisz program, który pobiera od użytkownika ciąg znaków i wyświetla liczbę samogłosek i spółgłosek w tym ciągu.
+* Poproś użytkownika o wprowadzenie liczby całkowitej w systemie dziesiętnym. Następnie skonwertuj tę liczbę na system dwójkowy (binarny) i wyświetl wynik.
+* Program sprawdzający czy podany ciąg znaków jest palindromem (czyli takim, który czytany od tyłu jest taki sam, jak czytany od przodu, np. "kajak")
+* Program sprawdzający czy podane dwa słowa są anagramami (czyli takimi, które zawierają te same litery, ale w innym układzie, np. "klasa" i "salka")
+* 
+
+* Program wyciągający informacje z numeru PESEL
+* Program implementujący algorytm szyfrowania Cezara (proste szyfrowanie, w którym każdy znak w tekście jest zastępowany innym znakiem, przesuniętym o stałą liczbę pozycji w alfabecie).
+
+
+* Program który na wejściu przyjmie równanie a na wyjściu da równanie w odwrotnej notacji polskiej ONP. Np. na wejściu 2+3*4 na wyjścu da 234*+
+* Program, który na wejściu przyjmie rówanie w ONP a na wyjściu wyświetli wynik rówania.
+*/
+
 //Napisz program, który wczyta znak z klawiatury i sprawdzi czy jest to mała litera aldfabetu.
 void task1()
 {
@@ -35,8 +51,31 @@ void task2()
     std::cout << "Witaj " << userName << " tutaj.\n";
 }
 
+//Napisz program, który policzy ile w napisie jest liter 'a'.
+void task3()
+{
+    std::string textFromUser;
+    std::cout << "Podaj tekst\n";
+    std::cin >> textFromUser;
+    
+    //textFromUser = "";
+    //textFromUser[0] = 'h';
+    //std::cout << textFromUser[1] << "\n";
+    //std::cout << textFromUser.length() << "\n"; 
+    //if (textFromUser == "Ala")
+    
+    int counter = 0;
+    for (int i = 0; i < textFromUser.length(); i++ )
+    {
+        if (textFromUser[i] == 'a')
+            counter++;
+    }
+
+    std::cout << "Małych liter 'a' jest " << counter << "\n";
+}
+
 int main()
 {
-    task2();
+    task3();
 }
 
