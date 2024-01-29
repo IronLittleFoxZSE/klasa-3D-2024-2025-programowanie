52ed8f38651a911f7593793ba620d97ae92b7a15
g <g@g.pl>
2024-01-29 11:36:29 +0100

    koniec programu

--- Programowanie/TicTacToeConsoleApplication/TicTacToeConsoleApplication.cpp
+++ Programowanie/TicTacToeConsoleApplication/TicTacToeConsoleApplication.cpp
@@ -45,6 +45,112 @@ void drawBoard(char board[], unsigned short size)
 		drawRow(board, size, row);
 		drawHorizontalFrame(size, row);
 	}
+
+}
+
+int GetPlaceOnBoard(char playerSign)
+{
+	int fieldNumber;
+	std::cout << "Gracz " << playerSign << " wykonuje ruch\n";
+	std::cout << "Podaj numer pola:\n";
+	std::cin >> fieldNumber;
+
+	return fieldNumber;
+}
+
+bool isWinInRow(char board[], unsigned short size, char currentPlayerSign, int row)
+{
+	for (int fieldInRow = 0; fieldInRow < size; fieldInRow++)
+	{
+		if (board[size * row + fieldInRow] != currentPlayerSign)
+			return false;
+	}
+	return true;
+}
+
+bool isWinInRows(char board[], unsigned short size, char currentPlayerSign)
+{
+	for (int row = 0; row < size; row++)
+	{
+		if (isWinInRow(board, size, currentPlayerSign, row))
+		     return true;
+	}
+	return false;
+}
+
+bool isWinInCol(char board[], unsigned short size, char currentPlayerSign, int col)
+{
+	for (int fieldInCol = 0; fieldInCol < size; fieldInCol++)
+	{
+		if (board[size * fieldInCol + col] != currentPlayerSign)
+			return false;
+	}
+	return true;
+}
+
+bool isWinInCols(char board[], unsigned short size, char currentPlayerSign)
+{
+	for (int col = 0; col < size; col++)
+	{
+		if (isWinInCol(board, size, currentPlayerSign, col))
+			return true;
+	}
+	return false;
+}
+
+bool isWinInLeftDiagonal(char board[], unsigned short size, char currentPlayerSign)
+{
+	for (int fieldInDiagonal = 0; fieldInDiagonal < size; fieldInDiagonal++)
+	{
+		if (board[size * fieldInDiagonal + fieldInDiagonal] != currentPlayerSign)
+			return false;;
+	}
+	return true;
+}
+
+bool isWinInRightDiagonal(char board[], unsigned short size, char currentPlayerSign)
+{
+	for (int fieldInDiagonal = 0; fieldInDiagonal < size; fieldInDiagonal++)
+	{
+		if (board[size * fieldInDiagonal + size - fieldInDiagonal - 1] != currentPlayerSign)
+			return false;;
+	}
+	return true;
+}
+
+bool isWinInDiagonals(char board[], unsigned short size, char currentPlayerSign)
+{
+	if (isWinInLeftDiagonal(board, size, currentPlayerSign))
+		return true;
+
+	if (isWinInRightDiagonal(board, size, currentPlayerSign))
+		return true;
+
+	return false;
+}
+
+bool isWin(char board[], unsigned short size, char currentPlayerSign)
+{
+	if (isWinInRows(board, size, currentPlayerSign))
+		return true;
+
+	if (isWinInCols(board, size, currentPlayerSign))
+		return true;
+
+	if (isWinInDiagonals(board, size, currentPlayerSign))
+		return true;
+
+	return false;
+}
+
+bool isDraw(char board[], unsigned short size)
+{
+	for (int i = 0; i < size * size; i++)
+	{
+		if (board[i] == ' ')
+			return false;
+	}
+	return true;
 }
 
 int main()
@@ -57,38 +163,37 @@ int main()
 	short currentPlayer = 0;
 
 	clearBoard(board, SIZE);
-	
-	//przygotawanie do gry
 
 	while (true)
 	{
 		int fieldNumber;
-		do
-		{
-			system("cls");
-			drawBoard(board, SIZE);
-			std::cout << "Gracz " << players[currentPlayer] << " wykonuje ruch\n";
-			std::cout << "Podaj numer pola:\n";
-			std::cin >> fieldNumber;
-		} while (board[fieldNumber] != ' ');
+		system("cls");
+		drawBoard(board, SIZE);
+
+		fieldNumber = GetPlaceOnBoard(players[currentPlayer]);
+
+		if (fieldNumber < 0
+			|| fieldNumber >= SIZE * SIZE
+			|| board[fieldNumber] != ' ')
+			continue;
 
 		board[fieldNumber] = players[currentPlayer];
 
+		system("cls");
 		drawBoard(board, SIZE);
 
-		//sprawdzenie czy aktualny gracz wygrał
+		if (isWin(board, SIZE, players[currentPlayer]))
 		{
-			//wyświelenie o wygraniej aktualnego gracza
-			//break;
+			std::cout << "Wygrana " << players[currentPlayer] << "\n";
+			break;
 		}
 
-		//sprawdzenie czy remis
+		if (isDraw(board, SIZE))
 		{
-			//wyświetlenie informacji o remisie
-			//break;
+			std::cout << "Reamis!!\n";
+			break;
 		}
 
-		//zmieniamy gracza na kolejnego
 		currentPlayer = (currentPlayer + 1) % NUMBER_OF_PLAYERS;
 	}
 }
