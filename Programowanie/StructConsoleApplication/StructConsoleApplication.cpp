323f5d1438fafe91a49e4f9c2cd3d032065b75e8
g <g@g.pl>
2024-02-02 13:19:44 +0100

    koniec lekcji

---
+++ Programowanie/StructConsoleApplication/StructConsoleApplication.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+struct point
+{
+	int x;
+	int y;
+	int z;
+};
+
+double DistanceFromCenter(int x, int y, int z)
+{
+	double distance;
+	distance = sqrt(x * x + y * y + z * z);
+	return distance;
+}
+
+double DistanceFromCenter(point p)
+{
+	double distance;
+	distance = sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
+	return distance;
+}
+
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+
+	int x, y, z;
+
+	std::cout << "Podaj x\n";
+	std::cin >> x;
+	std::cout << "Podaj y\n";
+	std::cin >> y;
+	std::cout << "Podaj z\n";
+	std::cin >> z;
+
+	std::cout << "Odległość " << DistanceFromCenter(x, y, z) << "\n";
+
+	int x1, y1;
+	int x2, y2;
+
+	int xx[50], yy[50], zz[50];
+	xx[3] = 5;
+	yy[3] = 7;
+	zz[3] = 77;
+
+	point firstPoint;
+	firstPoint.x = 3;
+	firstPoint.y = 5;
+	firstPoint.z = 8;
+	std::cout << "Odległość " << DistanceFromCenter(firstPoint.x, firstPoint.y, firstPoint.z) << "\n";
+	std::cout << "Odległość " << DistanceFromCenter(firstPoint) << "\n";
+
+	point tabOfPoints[15];
+	tabOfPoints[2].y = 68;
+}
