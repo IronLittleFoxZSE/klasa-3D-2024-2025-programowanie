1e800812309c0f8979705d2285db5185f37cf217
g <g@g.pl>
2024-02-26 11:48:07 +0100

    koniec lekcji

---
+++ Programowanie/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+class Point
+{
+private:
+	int x;
+	int y;
+	int z;
+
+public:
+	void SetX(int value)
+	{
+		//x = abs(value);
+		if (value >= 0)
+			x = value;
+		else
+		{
+			//reakcja na błąd
+		}
+	}
+
+	int GetX()
+	{
+		//if (uprawnienia)
+
+		return x;
+	}
+
+	double DistanceFromCenter()
+	{
+		double distance;
+		distance = sqrt(x * x + y * y + z * z);
+		return distance;
+	}
+};
+
+
+
+int main()
+{
+	Point firstPoint;
+	//firstPoint.x = 3;
+	firstPoint.SetX(-3);
+	//firstPoint.y = 5;
+	//firstPoint.z = 8;
+	std::cout << "Odległość " << firstPoint.DistanceFromCenter() << "\n";
+
+}
