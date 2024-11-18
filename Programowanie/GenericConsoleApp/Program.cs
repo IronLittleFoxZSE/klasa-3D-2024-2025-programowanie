8da2bfad00f1475de1899ef25a2615fc4b546780
f <f@f.pl>
2024-11-18 11:40:21 +0100

    omawianie typów generycznych

---
+++ Programowanie/GenericConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿
+/*
+void ShowValue(string value)
+{
+    Console.WriteLine(value);
+}
+void ShowValue(int value)
+{
+    Console.WriteLine(value);
+}
+void ShowValue(bool value)
+{
+    Console.WriteLine(value);
+}
+*/
+
+void ShowValue<T>(T value)
+{
+    Console.WriteLine(value);
+}
+
+string text = "Ala";
+ShowValue<string>(text);
+
+int number = 9;
+ShowValue/*<int>*/(number);
+
+bool boolValue = true;
+ShowValue(boolValue);
+
+
+//--------------------------------------
+/*
+void Swap(ref int x, ref int y)
+{
+    int tmp = x; 
+    x = y;
+    y = tmp;
+}
+
+void Swap(ref string x, ref string y)
+{
+    string tmp = x;
+    x = y;
+    y = tmp;
+}
+*/
+
+void Swap<T>(ref T x, ref T y)
+{
+    T tmp = x;
+    x = y;
+    y = tmp;
+}
+
+int firstNumber = 2, secondNumber = 5;
+Swap(ref firstNumber, ref secondNumber);
+
+string firstText = "Ala", secondText = "Ola";
+Swap(ref firstText, ref secondText);
--- Programowanie/LinqConsoleApp/Program.cs
+++ Programowanie/LinqConsoleApp/Program.cs
@@ -32,6 +32,20 @@ person.Age = 35;
 
 people.Add(person);
 
+person = new Person();
+person.Name = "Paweł";
+person.Surname = "Nowak";
+person.Age = 5;
+
+people.Add(person);
+
+person = new Person();
+person.Name = "Karol";
+person.Surname = "Nowak";
+person.Age = 23;
+
+people.Add(person);
+
 #endregion
 
 
@@ -73,3 +87,68 @@ double avgAge = people.Average(p => p.Age);
 Console.WriteLine($"Średnia wieku {avgAge} lat.");
 Console.WriteLine();
 
+Console.Clear();
+
+List<Person> sortedPeople = people.OrderBy(p => p.Surname).ThenByDescending(p => p.Age).ToList();
+/*
+select *  
+from people
+order by Surname asc, Age desc
+*/
+Console.WriteLine("Kolekcja posortowana:");
+foreach (Person p in sortedPeople)
+{
+    Console.WriteLine($"Imię: {p.Name} Nazwisko: {p.Surname} Wiek: {p.Age}");
+}
+Random rnd = new Random();
+List<Person> mixedPeople = people.OrderBy(p => rnd.Next()).ToList();
+
+/*
+select *  
+from people
+where Age < 18
+ */
+
+//if (people.Where(p => p.Age < 18).Count() > 0)
+if (people.Any(p => p.Age < 18))
+{
+    Console.WriteLine("Są osoby niepełnoletnie");
+}
+
+if (people.All(p => p.Age >= 18))
+{
+    Console.WriteLine("Wszystkie osoby są pełnoletnie");
+}
+
+Person firstPerson = people.FirstOrDefault(p => p.Name.Length == 3);
+//if (firstPerson != null) // jeżeli typ Person jest klasą
+if (firstPerson.Equals(default(Person)) == false) // sprawdzenie dla typu wartościowego
+{
+    Console.WriteLine("Osoba z imieniem większym niż trzy litery");
+    Console.WriteLine($"{firstPerson.Name}");
+}
+
+/*
+select Surname, count(*), max(Age)
+from people
+grooup by Surname
+*/
+
+var groupBySurname = people.GroupBy(p => p.Surname);
+Console.WriteLine("Pogrupowana kolekcja po nazwiskach");
+foreach(var group in groupBySurname)
+{
+    Console.WriteLine($"Nazwisko w grupie {group.Key} ");
+    Console.WriteLine($"Ilość osób w grupie: {group.Count()}");
+    Console.WriteLine($"Najstarsza odoba ma lat: {group.Max(p => p.Age)}");
+    Console.WriteLine("Elementy grupy:");
+    var sortGroup = group.OrderBy(group => group.Name);
+    foreach(Person p in sortGroup)
+    {
+        Console.WriteLine($"Imię: {p.Name} Nazwisko: {p.Surname} Wiek: {p.Age}");
+    }
+}
+
+
+
+Console.Clear();
