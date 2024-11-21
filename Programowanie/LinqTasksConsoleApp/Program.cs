cb9f6eeaba91cf6801ccecc7b0e8a4454d02f5b7
f <f@f.pl>
2024-11-21 10:12:10 +0100

    dodanie zadań

--- Programowanie/LinqTasksConsoleApp/Program.cs
+++ Programowanie/LinqTasksConsoleApp/Program.cs
@@ -10,3 +10,14 @@ var tasks = new List<TaskItem>
     new TaskItem(4, "Study LINQ", false),
     new TaskItem(5, "Exercise", true)
 };
+
+/*
+var tasks = new List<TaskItem>();
+tasks.Add(new TaskItem(1, "Buy groceries", true));
+tasks.Add(new TaskItem(2, "Clean the house", false));
+//itd.
+*/
+
+// Zadanie 1: Wyszukaj wszystkie zakończone zadania
+
+// Zadanie 2: Znajdź pierwsze zadanie, które nie jest zakończone
