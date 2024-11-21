d40d967645df82bf6ed412f1703770d622b9ec54
f <f@f.pl>
2024-11-21 10:14:55 +0100

    dodanie kolejnych zadań

--- Programowanie/LinqTasksConsoleApp/Program.cs
+++ Programowanie/LinqTasksConsoleApp/Program.cs
@@ -21,3 +21,19 @@ tasks.Add(new TaskItem(2, "Clean the house", false));
 // Zadanie 1: Wyszukaj wszystkie zakończone zadania
 
 // Zadanie 2: Znajdź pierwsze zadanie, które nie jest zakończone
+
+// Zadanie 3: Posortuj zadania według nazwy
+
+// Zadanie 4: Policz zadania zakończone
+
+// Zadanie 5: Wybierz tylko nazwy zadań i wyświetl
+
+// Zadanie 6: Znalezienie nazw zakończonych zadań posortowanych według długości nazwy
+
+//Zadanie 7: Zadania pogrupowane według stanu zakończenia, a następnie posortowane w grupach według nazwy
+
+//Zadanie 8: Najkrótsza nazwa zadania niezakończonego
+
+//Zadanie 9: Ilość liter w nazwach wszystkich zakończonych zadań
+
+//Zadanie 10: Lista zadań z indeksami (zakończone zadania z numeracją)
