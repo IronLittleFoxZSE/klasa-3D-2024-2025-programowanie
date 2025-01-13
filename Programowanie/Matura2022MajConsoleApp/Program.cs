a0519e2fbab1833714502c9985f0d7ae7187db6b
f <f@f.pl>
2025-01-13 11:11:12 +0100

    rozwiązanie zadania 4.1 z matury maj 2022

---
+++ Programowanie/Matura2022MajConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+//Matura2022MajConsoleApp
+
+StreamReader streamReader = new StreamReader("przyklad.txt");
+
+string strNumberFromFile;
+List<string> strNumbers = new List<string>();
+
+while ((strNumberFromFile = streamReader.ReadLine()) != null)
+{
+    strNumbers.Add(strNumberFromFile);
+}
+
+streamReader.Close();
+
+//zadanie 4.1
+int counter4_1 = strNumbers.Where(s => s[0] == s[s.Length - 1]).Count();
+string firstNumber4_1 = strNumbers.First(s => s[0] == s[s.Length - 1]);
+Console.WriteLine($"{counter4_1} {firstNumber4_1}");
+
