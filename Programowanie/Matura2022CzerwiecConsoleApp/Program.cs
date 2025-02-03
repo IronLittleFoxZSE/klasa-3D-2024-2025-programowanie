e93b3022eeb6c1619659c0d131e80ab71700aadf
f <f@f.pl>
2025-02-03 11:48:15 +0100

    wykonanie zadania 4.1

---
+++ Programowanie/Matura2022CzerwiecConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿StreamReader streamReader = new StreamReader("przyklad.txt");
+
+string strNumberFromFile;
+List<string> strNumbers = new List<string>();
+
+while ((strNumberFromFile = streamReader.ReadLine()) != null)
+{
+    strNumbers.Add(strNumberFromFile);
+}
+
+streamReader.Close();
+
+//Zadanie 4.1
+var reverseDovidedBy17 = strNumbers
+    .Where(s => int.Parse(string.Join("", s.Reverse())) % 17 == 0)
+    .Select(s => string.Join("", s.Reverse()));
+
+Console.WriteLine("Zdanie 4.1:");
+foreach (var strNumber in reverseDovidedBy17)
+    Console.WriteLine(strNumber);
